905519689dbb8eee857217ae7dcee2e642ff4874
HuyQuoc367 <quochuyvipvl1999@gmail.com>
2022-06-01 21:00:31 +0700

    update lasles project

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,8 @@ $('.customer-list').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     dots: true,
-    prevArrow: `<button type='button' class='slick-prev pull-left'><img src="../images/arrow-left.svg" /></button>`,
-    nextArrow: `<button type='button' class='slick-next pull-right'><img src="../images/arrow-right.svg" /></button>`,
+    prevArrow: `<button type='button' class='slick-prev pull-left'><img src="./images/arrow-left.svg" /></button>`,
+    nextArrow: `<button type='button' class='slick-next pull-right'><img src="./images/arrow-right.svg" /></button>`,
     responsive: [
         {
           breakpoint: 1024,
